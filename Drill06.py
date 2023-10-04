458af4a28d07366fbf4bea9a01b2945f4015e849
gamejoon <gamejoon0325@naver.com>
2023-10-04 23:12:37 +0900

    목표점들을 차례로 이동하는 소년 구현 완료.

--- Drill06.py
+++ Drill06.py
@@ -42,13 +42,21 @@ def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
-    sx, sy = cx, cy
-    # hx, hy = 50, 50
-    hx, hy = points[0] # p2: 끝점
-    t = 0.0
-    action = 1 if sx < hx else 0
-    frame = 0
+    global target_exists
 
+    if points: # points 리스트 안에 남아있는 점이 있으면
+        sx, sy = cx, cy
+        # hx, hy = 50, 50
+        hx, hy = points[0] # p2: 끝점
+        t = 0.0
+        action = 1 if sx < hx else 0
+        frame = 0
+        target_exists = True
+    else:
+        action = 3 if action == 1 else 2 # 이전에 소년이 우측으로 이동중이었으면, IDLE 동작 시 우측을 바라보도록
+        frame = 0
+        target_exists = False
+        
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -65,13 +73,15 @@ def update_world():
 
     frame = (frame + 1) % 8
     
-    if t <= 1.0:
-        cx = (1 - t) * sx + t * hx # cx는 시작 x 와 끝 x 를 1 - t : t 의 비율로 섞은 위치
-        cy = (1 - t) * sy + t * hy
-        t = t + 0.001
-    else:
-        cx, cy = hx, hy # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴.
-        set_new_target_arrow()
+    if target_exists:
+        if t <= 1.0:
+            cx = (1 - t) * sx + t * hx # cx는 시작 x 와 끝 x 를 1 - t : t 의 비율로 섞은 위치
+            cy = (1 - t) * sy + t * hy
+            t = t + 0.001
+        else: # 목표지점에 도달하면
+            cx, cy = hx, hy # 캐릭터 위치를 기존 목적지 위치와 강제로 정확히 일치시킴.
+            del points[0] # 목표지점에 왔기 때문에, 더이상 필요없는 점을 삭제
+            set_new_target_arrow()
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
